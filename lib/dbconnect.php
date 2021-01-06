668450d9a7929b67057c8554e319f13e6eb4c842
TasosMil <73336408+TasosMil@users.noreply.github.com>
2021-01-06 19:06:09 +0200

    updated db_connect

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -8,7 +8,14 @@ $username=$DB_USER;
 $password=$DB_PASS;
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+if(gethostname()=='users.iee.ihu.gr')
+{
+  $conn = new mysqli($servername, $username, $password, $dbname,null,'/home/student/it/2015/it154510/mysql/run/mysql.sock');
+}
+else
+{
+  $conn = new mysqli($servername, $username, $password, $dbname);
+}
 
 
 if ($conn->connect_error) {
